ad779187038e8276f12470a7ce95b2f6ac59a55f
Erick Christian <erickchristian48@gmail.com>
2025-02-07 18:29:10 +0700

    refactor: Simplify parameter access in createSSMLString function

--- src/lib/ssml.ts
+++ src/lib/ssml.ts
@@ -9,16 +9,15 @@ export interface SSMLParams {
 }
 
 export function createSSMLString(params: SSMLParams): string {
-  const { requestId, text, voice, language, rate, pitch, volume } = params
   return `
-  X-RequestId:${requestId}\r\n
+  X-RequestId:${params.requestId}\r\n
   Content-Type:application/ssml+xml\r\n
   Path:ssml\r\n\r\n
 
-  <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="${language}">
-    <voice name="${voice}">
-      <prosody rate="${rate}" pitch="${pitch}" volume="${volume}">
-        ${text}
+  <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="${params.language}">
+    <voice name="${params.voice}">
+      <prosody rate="${params.rate}" pitch="${params.pitch}" volume="${params.volume}">
+        ${params.text}
       </prosody>
     </voice>
   </speak>
